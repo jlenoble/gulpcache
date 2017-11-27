3039f217925fafa4c537a1180b6419b7cd825caa
Jason Lenoble <jason.lenoble@gmail.com>
2017-11-27 15:39:29 +0100

    Extracted property setting to own file

--- src/gulptask.js
+++ src/gulptask.js
@@ -1,133 +1,24 @@
 import {SingletonFactory} from 'singletons';
-import GulpStream from 'gulpstream';
 import gulp from 'gulp';
-import destglob from 'destglob';
 import DependencyMap from './dependency-map';
-import {makeFn, makeExecFn, makeWatchFn} from './function-factories';
+import {getName, setMainProperties, mixInStreamerProperties,
+  setFunctionProperties} from './properties';
 
 const dependencies = new DependencyMap();
 
-const getName = args => {
-  let name;
-
-  args.some(arg => {
-    if (typeof arg === 'string') {
-      name = arg;
-      return true;
-    }
-
-    if (arg.name) {
-      name = arg.name;
-      return true;
-    }
-
-    return false;
-  });
-
-  return name;
-};
-
-const getDescription = args => {
-  let description;
-
-  args.some(arg => {
-    if (arg.description) {
-      description = arg.description;
-      return true;
-    }
-
-    return false;
-  });
-
-  return description;
-};
-
-const getDependsOn = args => {
-  let dependsOn;
-
-  args.some(arg => {
-    if (arg.dependsOn) {
-      dependsOn = arg.dependsOn;
-      return true;
-    }
-
-    return false;
-  });
-
-  if (!dependsOn) {
-    return [];
-  }
-
-  return Array.isArray(dependsOn) ? dependsOn : [dependsOn];
-};
-
 export class SimpleGulpTask {
   constructor (...args) {
-    const name = getName(args);
-    const description = getDescription(args);
-    const dependsOn = getDependsOn(args);
+    setMainProperties(this, args);
 
-    const _streamer = (new GulpStream(args)).at(0);
+    mixInStreamerProperties(this);
 
-    Object.defineProperties(this, {
-      name: {
-        value: name,
-      },
-
-      description: {
-        value: description,
-      },
-      // 
-      // dependsOn: {
-      //   value: dependsOn,
-      // },
-      //
-      // glob: {
-      //   value: _streamer.glob,
-      // },
-      //
-      // destglob: {
-      //   value: _streamer.destination ? destglob(_streamer.glob,
-      //     _streamer.destination) : null,
-      // },
-      //
-      // plugin: {
-      //   value: _streamer.plugin,
-      // },
-      //
-      // dest: {
-      //   value: _streamer.destination,
-      // },
-      //
-      // streamer: {
-      //   value: _streamer,
-      // },
-    });
-
-    // Factories rely on nampe and descripton properties to be defined
-    const fn = makeFn(args, this);
-    const execFn = makeExecFn(this);
-    const watchFn = makeWatchFn(this);
-
-    Object.defineProperties(this, {
-      fn: {
-        value: fn,
-      },
-
-      execFn: {
-        value: execFn,
-      },
-
-      watchFn: {
-        value: watchFn,
-      },
-    });
+    setFunctionProperties(this, args);
 
     dependencies.register(this);
 
-    gulp.task(execFn);
-    gulp.task(watchFn);
-    gulp.task(`tdd:${name}`, gulp.series(execFn, watchFn));
+    gulp.task(this.execFn);
+    gulp.task(this.watchFn);
+    gulp.task(`tdd:${this.name}`, gulp.series(this.execFn, this.watchFn));
   }
 
   getDependencies () {
---
+++ src/properties.js
@@ -0,0 +1,118 @@
+import GulpStream from 'gulpstream';
+import destglob from 'destglob';
+import {makeFn, makeExecFn, makeWatchFn} from './function-factories';
+
+const getName = args => {
+  let name;
+
+  args.some(arg => {
+    if (typeof arg === 'string') {
+      name = arg;
+      return true;
+    }
+
+    if (arg.name) {
+      name = arg.name;
+      return true;
+    }
+
+    return false;
+  });
+
+  return name;
+};
+
+const getDescription = args => {
+  let description;
+
+  args.some(arg => {
+    if (arg.description) {
+      description = arg.description;
+      return true;
+    }
+
+    return false;
+  });
+
+  return description;
+};
+
+const getDependsOn = args => {
+  let dependsOn;
+
+  args.some(arg => {
+    if (arg.dependsOn) {
+      dependsOn = arg.dependsOn;
+      return true;
+    }
+
+    return false;
+  });
+
+  if (!dependsOn) {
+    return [];
+  }
+
+  return Array.isArray(dependsOn) ? dependsOn : [dependsOn];
+};
+
+const setMainProperties = (ctx, args) => {
+  Object.defineProperties(ctx, {
+    name: {
+      value: getName(args),
+    },
+
+    description: {
+      value: getDescription(args),
+    },
+
+    dependsOn: {
+      value: getDependsOn(args),
+    },
+
+    streamer: {
+      value: (new GulpStream(args)).at(0),
+    },
+  });
+};
+
+const mixInStreamerProperties = ctx => {
+  Object.defineProperties(ctx, {
+    glob: {
+      value: ctx.streamer.glob,
+    },
+
+    destglob: {
+      value: ctx.streamer.destination ? destglob(ctx.streamer.glob,
+        ctx.streamer.destination) : null,
+    },
+
+    plugin: {
+      value: ctx.streamer.plugin,
+    },
+
+    dest: {
+      value: ctx.streamer.destination,
+    },
+  });
+};
+
+const setFunctionProperties = (ctx, args) => {
+  // Factories rely on ctx's main properties to already be defined
+  Object.defineProperties(ctx, {
+    fn: {
+      value: makeFn(args, ctx),
+    },
+
+    execFn: {
+      value: makeExecFn(ctx),
+    },
+
+    watchFn: {
+      value: makeWatchFn(ctx),
+    },
+  });
+};
+
+export {getName, setMainProperties, mixInStreamerProperties,
+  setFunctionProperties};
