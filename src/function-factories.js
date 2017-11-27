48beb77249e881c408f62b9056c74d1449c37f44
Jason Lenoble <jason.lenoble@gmail.com>
2017-11-27 15:12:20 +0100

    Extracted function factories to own file

---
+++ src/function-factories.js
@@ -0,0 +1,108 @@
+import gulp from 'gulp';
+import del from 'del';
+import destglob from 'destglob';
+
+const makeFn = (args, ctx) => {
+  // Base action for task
+  let fn;
+
+  args.some(arg => {
+    if (arg.fn) {
+      fn = (...args) => arg.fn(...args);
+      return true;
+    }
+
+    return false;
+  });
+
+  if (!fn) {
+    if (ctx.dest) {
+      fn = () => ctx.streamer.dest().isReady();
+    } else {
+      fn = () => ctx.streamer.stream();
+    }
+  }
+
+  const f = (...args) => {
+    const fns = ctx.getDependents().map(task => task.fn);
+
+    if (fns.length) {
+      return gulp.series(fn, gulp.parallel(...fns))(...args);
+    }
+
+    return fn(...args);
+  };
+
+  Object.defineProperties(f, {
+    name: {
+      value: ctx.name,
+    },
+
+    description: {
+      value: ctx.description,
+    },
+  });
+
+  return f;
+};
+
+const makeExecFn = ctx => {
+  // Base action for task preceded by all required actions
+  // For this, implicit and explicit deps are the same
+  const f = (...args) => {
+    const execFns = ctx.getDependencies().map(task => task.execFn);
+
+    if (execFns.length) {
+      return gulp.series(gulp.parallel(...execFns), ctx.fn)(...args);
+    }
+
+    return ctx.fn(...args);
+  };
+
+  Object.defineProperties(f, {
+    name: {
+      value: `exec:${ctx.name}`,
+    },
+
+    description: {
+      value: `Executing task ${ctx.name}`,
+    },
+  });
+
+  return f;
+};
+
+const makeWatchFn = ctx => {
+  // Watching to trigger base action
+  // Also setting watch on deps, implicit and explicit alike
+  // Implicit will trigger back the task (say source files have changed)
+  // But explicit must force action again (say config files have changed)
+  const f = done => {
+    const watcher = gulp.watch(ctx.glob, ctx.fn);
+    watcher.on('unlink', file => {
+      if (ctx.dest) {
+        return del(destglob(file, ctx.dest));
+      }
+    });
+
+    ctx.getDependencies().map(task => task.watchFn).forEach(fn => fn());
+
+    if (done) {
+      done();
+    }
+  };
+
+  Object.defineProperties(f, {
+    name: {
+      value: `watch:${ctx.name}`,
+    },
+
+    description: {
+      value: `Watching task ${ctx.name}`,
+    },
+  });
+
+  return f;
+};
+
+export {makeFn, makeExecFn, makeWatchFn};
--- src/gulptask.js
+++ src/gulptask.js
@@ -2,8 +2,8 @@ import {SingletonFactory} from 'singletons';
 import GulpStream from 'gulpstream';
 import gulp from 'gulp';
 import destglob from 'destglob';
-import del from 'del';
 import DependencyMap from './dependency-map';
+import {makeFn, makeExecFn, makeWatchFn} from './function-factories';
 
 const dependencies = new DependencyMap();
 
@@ -61,73 +61,6 @@ const getDependsOn = args => {
   return Array.isArray(dependsOn) ? dependsOn : [dependsOn];
 };
 
-const makeFn = (args, ctx) => {
-  // Base action for task
-  let fn;
-
-  args.some(arg => {
-    if (arg.fn) {
-      fn = (...args) => arg.fn(...args);
-      return true;
-    }
-
-    return false;
-  });
-
-  if (!fn) {
-    if (ctx.dest) {
-      fn = () => ctx.streamer.dest().isReady();
-    } else {
-      fn = () => ctx.streamer.stream();
-    }
-  }
-
-  return (...args) => {
-    const fns = ctx.getDependents().map(task => task.fn);
-
-    if (fns.length) {
-      return gulp.series(fn, gulp.parallel(...fns))(...args);
-    }
-
-    return fn(...args);
-  };
-};
-
-const makeExecFn = ctx => {
-  // Base action for task preceded by all required actions
-  // For this, implicit and explicit deps are the same
-  return (...args) => {
-    const execFns = ctx.getDependencies().map(task => task.execFn);
-
-    if (execFns.length) {
-      return gulp.series(gulp.parallel(...execFns), ctx.fn)(...args);
-    }
-
-    return ctx.fn(...args);
-  };
-};
-
-const makeWatchFn = ctx => {
-  // Watching to trigger base action
-  // Also setting watch on deps, implicit and explicit alike
-  // Implicit will trigger back the task (say source files have changed)
-  // But explicit must force action again (say config files have changed)
-  return done => {
-    const watcher = gulp.watch(ctx.glob, ctx.fn);
-    watcher.on('unlink', file => {
-      if (ctx.dest) {
-        return del(destglob(file, ctx.dest));
-      }
-    });
-
-    ctx.getDependencies().map(task => task.watchFn).forEach(fn => fn());
-
-    if (done) {
-      done();
-    }
-  };
-};
-
 export class SimpleGulpTask {
   constructor (...args) {
     const name = getName(args);
@@ -144,68 +77,37 @@ export class SimpleGulpTask {
       description: {
         value: description,
       },
-
-      dependsOn: {
-        value: dependsOn,
-      },
-
-      glob: {
-        value: _streamer.glob,
-      },
-
-      destglob: {
-        value: _streamer.destination ? destglob(_streamer.glob,
-          _streamer.destination) : null,
-      },
-
-      plugin: {
-        value: _streamer.plugin,
-      },
-
-      dest: {
-        value: _streamer.destination,
-      },
-
-      streamer: {
-        value: _streamer,
-      },
+      // 
+      // dependsOn: {
+      //   value: dependsOn,
+      // },
+      //
+      // glob: {
+      //   value: _streamer.glob,
+      // },
+      //
+      // destglob: {
+      //   value: _streamer.destination ? destglob(_streamer.glob,
+      //     _streamer.destination) : null,
+      // },
+      //
+      // plugin: {
+      //   value: _streamer.plugin,
+      // },
+      //
+      // dest: {
+      //   value: _streamer.destination,
+      // },
+      //
+      // streamer: {
+      //   value: _streamer,
+      // },
     });
 
+    // Factories rely on nampe and descripton properties to be defined
     const fn = makeFn(args, this);
-
-    Object.defineProperties(fn, {
-      name: {
-        value: name,
-      },
-
-      description: {
-        value: description,
-      },
-    });
-
     const execFn = makeExecFn(this);
-
-    Object.defineProperties(execFn, {
-      name: {
-        value: `exec:${name}`,
-      },
-
-      description: {
-        value: `Executing task ${name}`,
-      },
-    });
-
     const watchFn = makeWatchFn(this);
-
-    Object.defineProperties(watchFn, {
-      name: {
-        value: `watch:${name}`,
-      },
-
-      description: {
-        value: `Watching task ${execFn.name}`,
-      },
-    });
 
     Object.defineProperties(this, {
       fn: {
